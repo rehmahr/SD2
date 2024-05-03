36e9fb80b93cd134e3d8c79cea5947951681b0ab
rehmahr <87372589+rehmahr@users.noreply.github.com>
2024-05-03 15:41:05 +0400

    changes

--- app/src/main/java/com/example/sd2/Game2Lev03.kt
+++ app/src/main/java/com/example/sd2/Game2Lev03.kt
@@ -1,16 +1,16 @@
+package com.example.sd2
+
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.ImageButton
-import android.widget.Toast
 import android.widget.VideoView
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import com.example.sd2.Dashboard
-import com.example.sd2.Game2lev12
-import com.example.sd2.R
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 class Game2Lev03 : AppCompatActivity() {
     private lateinit var videoView: VideoView
--- app/src/main/java/com/example/sd2/homepage.kt
+++ app/src/main/java/com/example/sd2/homepage.kt
@@ -1,6 +1,7 @@
 package com.example.sd2
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +12,17 @@ class homepage : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_homepage)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val buttonLogin = findViewById<Button>(R.id.button2)
+        val buttonReg = findViewById<Button>(R.id.register)
+
+        buttonLogin.setOnClickListener {
+            goToNextActivity(this, MainActivity::class.java)
         }
+
+        buttonReg.setOnClickListener {
+            goToNextActivity(this, Registration::class.java)
+        }
+
     }
 }
